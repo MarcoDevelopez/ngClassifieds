2f71fd86546986a730a78983ee16ea5bbc6e904d
Marco López <malopez@bethel.net>
2016-10-13 17:01:36 -0500

    Filtering Data and Others changes

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,46 +4,84 @@
 
 	angular
 		.module("ngClassifieds")
-		.controller("classifiedsCtrl", function ($scope) {
+		.controller("classifiedsCtrl", function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
-			$scope.classifieds = [
-				{
-					"id": "1",
-					"title": "20 Foot Equipment Trailer",
-					"description": "2013 rainbow trailer 20 feet x 82 inch deck area.",
-					"price": 6000,
-					"posted": "2015-10-24",
-					"contact": {
-						"name": "John Doe",
-						"phone": "(555) 555-5555",
-						"email": "jongdoe@gmail.com"
-					},
-					"categories": [
-						"Vehicles",
-						"Parts and Accesories"
-					],
-					"image": "http://www.fancyicons.com/free-icons/214/container/png/256/cai_truck_256.png",
-					"views": 123
-				},
-				{
-					"id": "2",
-					"title": "23 Foot Equipment Trailer",
-					"description": "2014 rainbow trailer 20 feet x 82 inch deck area.",
-					"price": 6500,
-					"posted": "2015-10-24",
-					"contact": {
-						"name": "Oscar Huipa",
-						"phone": "(555) 555-5552",
-						"email": "oscarhuipa23@gmail.com"
-					},
-					"categories": [
-						"Vehicles",
-						"Parts and Accesories"
-					],
-					"image": "http://www.fancyicons.com/free-icons/214/container/png/256/cai_truck_256.png",
-					"views": 123
+			classifiedsFactory.getClassifieds().then(function (classifieds) {
+				$scope.classifieds = classifieds.data;
+				$scope.categories = getCategories($scope.classifieds);
+			});
+
+			var contact = {
+				name: "Marco López",
+				phone: "45545545",
+				email: "mlopez@betheltv.pe"
+			}	
+
+			$scope.openSidebar = function () {
+				$mdSidenav('left').open();
+			}
+
+			$scope.closeSidebar = function () {
+				$mdSidenav('left').close();
+			}
+
+			$scope.saveClassified = function (classified) {
+				if (classified) {
+					classified.contact = contact;
+					$scope.classifieds.push(classified);
+					$scope.classified = {};
+					$scope.closeSidebar();
+					showToast("Classified saved!");
 				}
-			]
-			
+			}
+
+			$scope.editClassified = function (classified) {
+				$scope.editing = true;
+				$scope.openSidebar();
+				$scope.classified = classified;
+			}
+
+			$scope.saveEdit = function () {
+				$scope.editing = false;
+				$scope.classified = {};
+				$scope.closeSidebar();
+				showToast("Edit saved!");
+			}
+
+			$scope.deleteClassified = function (event, classified) {
+				var confirm = $mdDialog.confirm()
+					.title('Are you sure you want to delete ' + classified.title + '?')
+					.ok('Yes')
+					.cancel('No')
+					.targetEvent(event);
+				$mdDialog.show(confirm).then(function () {
+					var index = $scope.classifieds.indexOf(classified);
+					$scope.classifieds.splice(index, 1);
+				}, function () {
+
+				});
+			}
+
+			function showToast(message) {
+				$mdToast.show(
+						$mdToast.simple()
+							.content(message)
+							.position('top, right')
+							.hideDelay(3000)
+					);
+			}
+
+			function getCategories(classifieds) {
+				var categories = [];
+
+				angular.forEach(classifieds, function (item) {
+					angular.forEach(item.categories, function (category) {
+						categories.push(category);
+					});
+				});
+
+				return _.uniq(categories);
+			}
+
 		});
 })();
